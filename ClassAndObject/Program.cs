5b2ac22b1c43a0e18456ace42700d795bb40ffa9
Sajan Bista <162802857+SajanBista@users.noreply.github.com>
2025-05-11 22:59:44 +0545

    Add files via upload
    each of small  practices are done by making project so must of them are default file

---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,43 @@
+﻿//Abstract classes = can't make object we should access  it making derived class, means only be accessed  by inheriting in its child class
+
+using System;
+
+public abstract class Animal
+{
+    //Abstract method (must be implemented in derived class)
+    public abstract void makeSound();
+
+    public void sleep()
+    {
+        Console.WriteLine("Zzzzzzz!!!"); 
+    }
+}
+
+public class Dog : Animal
+{
+    public void walk()
+    {
+        Console.WriteLine("dog is walking"); 
+    }
+
+   
+    public override void makeSound()
+    {
+        Console.WriteLine("Woof! Woof!");
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        // Create a dog object
+        Dog dog = new Dog();
+        
+        // Call methods
+        dog.makeSound();
+        dog.walk();
+        dog.sleep();
+    }
+}
+//creates layer of abstraction
---
+++ PartialClass/myclass2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace PartialClass
+{
+    public partial class MyClass
+    {
+        public void Method2()
+        {
+            Console.WriteLine("Method2 from MyClass");
+        }
+    }
+}
